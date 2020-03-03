9f266c759840f3c000be56c04f2f076efc460e06
pransharma <pransharma@CAW181555.ca.deloitte.com>
2020-03-03 10:25:30 -0500

    +PS - Indenting changes to NthPrimeNumber

--- StringManipulations/src/problems/NthPrimeNumber.java
+++ StringManipulations/src/problems/NthPrimeNumber.java
@@ -17,11 +17,17 @@ public class NthPrimeNumber {
     }
     
     public static boolean isPrime(long n) {
-        if (n < 2) return false;
-        else if (n == 2) return true;
-        for (int i = 2; i < Math.pow(n, 0.5) + 1; i++)
-            if (n % i == 0)
+        if (n < 2) {
+        	return false;
+        }
+        else if (n == 2) {
+        	return true;
+        }
+        for (int i = 2; i < Math.pow(n, 0.5) + 1; i++) {
+            if (n % i == 0) {
                 return false;
+            }
+        }
         return true;
     }
 }
